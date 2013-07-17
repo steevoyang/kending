ccf5e599e47fa4f2f8aba375db26b361331629d3
Stephen Yang <steevoyang@gmail.com>
2013-07-17 00:21:54 -0400

    added social and highlighting links in nav bar with scrolling

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
  	var navbar = $('.nav-bar');
  	var minHeight; // minHeight is also the offset for the navbar
 
+ 	// fixes nav bar when reaches top of window
  	$(window).scroll(function () {
  		var navbarScrolltop = $(this).scrollTop();
  		if (navbarScrolltop >= minHeight) {
@@ -12,6 +13,7 @@ $(document).ready(function() {
  		}
 	});
 	
+	// smooth scrolls to each section
 	$('.nav-items li').click(function () {
 		var sectionName = $(this).attr('section');
 		var sectionElement = $('.'+sectionName);
@@ -19,6 +21,44 @@ $(document).ready(function() {
 		$('html, body').animate({scrollTop: sectionOffset}, 'slow');
 	});
 
+
+	// Cache selectors
+	var lastId;
+	var navItems = $(".nav-items");
+	var navItemsHeight = navItems.outerHeight()+15;
+	var menuItems = navItems.find("a"); // All list items
+	    // Sections corresponding to menu items
+	    scrollItems = menuItems.map(function(){
+			var item = $($(this).attr("href"));
+			if (item.length) { return item; }
+	    });
+
+	// Bind to scroll
+	$(window).scroll(function(){
+		// Get container scroll position
+		var fromTop = $(this).scrollTop()+navItemsHeight;
+   
+	// Get id of current scroll item
+	var cur = scrollItems.map(function(){
+		if ($(this).offset().top < fromTop)
+		return this;
+	});
+
+	// Get the id of the current element
+	cur = cur[cur.length-1];
+	var id = cur && cur.length ? cur[0].id : "";
+   
+	   if (lastId !== id) {
+	       lastId = id;
+	       // Set/remove active class
+	       menuItems
+	         .parent().removeClass("active")
+	         .end().filter("[href=#"+id+"]").parent().addClass("active");
+	   }
+	});    
+
+
+	// calculates resizing window and sets site header site to that
 	function onResize() {
 		minHeight = $(window).height();
 		$('.site-header').css('min-height', minHeight + 'px');
@@ -26,11 +66,6 @@ $(document).ready(function() {
 
 	onResize();
 	$(window).bind('resize', onResize);
-
-
-	// AnyStretch on Homepage background image
-	// $(".site-header").anystretch("http://f.cl.ly/items/2N1W1F0F2P3T0i2c2k1n/Untitled-1.png");
-
 
 	// Settign variables for video
 	var video = $(".intro-video");
@@ -48,7 +83,9 @@ $(document).ready(function() {
 		html.addClass("video-is-visible");
 		video.attr('src', 'http://player.vimeo.com/video/70332615?autoplay=1&amp;player_id=vimeoplayer&amp;api=1');
 		$('.intro-video').css('min-height', minHeight);
-		$('.close-video').css('opacity', minHeight);
+		$('html').append('<style type="text/css">.site-header:hover button{opacity: 1;}</style>');
+		// $('.site-header:hover button').css('opacity', 1);
+		// $('.close-video').css('opacity', 1);
     });
 
     // Hide the video
@@ -56,7 +93,6 @@ $(document).ready(function() {
 	    e.preventDefault();
 	    html.removeClass("video-is-visible");
 	    video.attr("src", "");
+	    $('.close-video').css('opacity', 0);
     });
-
-
 });
